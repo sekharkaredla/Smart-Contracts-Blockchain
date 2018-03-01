1a1d0f8a79899b450e7f3ccb534c7955c516da33
MohammedAijaaz <mdaijaaz@outlook.com>
2018-03-01 14:44:17 +0530

    Added Styles

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ abi = JSON.parse(
   '[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votedList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"voterHashNew","type":"bytes32"}],"name":"validVoter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"},{"name":"voterHash","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]'
 );
 VotingContract = web3.eth.contract(abi);
-// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
+
 contractInstance = VotingContract.at(
-  "0xa84eb3d8e406d1f98d8a17ef738412551743ef83"
+  "0x8935bbefe12396f98771ae7f1c2aa313572d383b"
 );
-candidates = { Aijaaz: "candidate-1", Sekhar: "candidate-2", Pranith: "candidate-3" };
+candidates = {
+  Aijaaz: "candidate-1",
+  Sekhar: "candidate-2",
+  Pranith: "candidate-3"
+};
 
 function voteForCandidate() {
   candidateName = $("#candidate").val();

f9df9ccebd6f445e5df9c8e2c25bdad77d3f3688
sekharkaredla <sekhar.karedla@gmail.com>
2018-05-09 05:21:34 -0700

    running node server

--- node_server_start.js
+++ node_server_start.js
@@ -10,16 +10,24 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
-con.query('SELECT * FROM event_details WHERE node_server=1', (err,rows) => {
+function nodeCreate(){
+connection.query('SELECT * FROM event_details WHERE node_server=1', (err,rows) => {
   if(err) throw err;
 
-  console.log('Data received from Db:\n');
-  console.log(rows);
-  var event_id=rows['event_id'];
-
-
-shell.exec('pm2 start nodesetup.js -- '+event_id, function(code, output) {
+  // console.log('Data received from Db:\n');
+  // console.log(rows);
+  for(var each = 0; each < rows.length; each++)
+  {
+    // console.log(rows[each]['event_id']);
+shell.exec('pm2 start nodesetup.js -- '+rows[each]['event_id'], function(code, output) {
   console.log('Exit code:', code);
   console.log('Program output:', output);
 });
+  }
 });
+connection.query('update event_details set node_server=0', (err,rows) => {
+  if(err) throw err;
+    });
+setTimeout(nodeCreate,5*1000);
+}
+nodeCreate();

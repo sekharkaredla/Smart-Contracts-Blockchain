1b8eeacc308e082ce7d5684a9bfa0d547ea90a89
pranithkumar <praneeth970@gmail.com>
2018-05-15 18:20:15 +0530

    end vote

--- end_event.php
+++ end_event.php
@@ -7,7 +7,7 @@
 	if($conn->connect_error){
 		die('connection failed '.$conn->$connect_error);
 	}
-	$sql = 'update event_details set running=true where event_id='.'$_GET['event_id']';
+	$sql = 'update event_details set running=true where event_id='.'"$_GET['event_id']"';
   echo $sql;
   if ($conn->query($sql) === FALSE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
